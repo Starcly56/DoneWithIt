e0c4cd71d0fca86d35124db8c25dbf1a6b6c568b
Starcly56 <humagainuzjwol@gmail.com>
2020-02-15 16:25:46 +0545

    added todos and graphics

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import Todos from "./components/Todos";
 
 export default function App() {
+  const [todo, setTodo] = useState();
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleAddTodo = () => {
+    Keyboard.dismiss();
+    setTodoItems([...todoItems, todo]);
+    setTodo(null);
+  };
+
+  const completeTodo = (index) => {
+    let itemsCopy = [...todoItems];
+    itemsCopy.splice(index, 1);
+    setTodoItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello Ujjwal</Text>
-      <StatusBar style="auto" />
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today's todos</Text>
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {todoItems.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => completeTodo(index)}
+                >
+                  <Todos text={item} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a todo"}
+          value={todo}
+          onChangeText={(text) => setTodo(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTodo()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +75,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    fontSize: 18,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
     justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {
+    fontSize: 25,
   },
 });
